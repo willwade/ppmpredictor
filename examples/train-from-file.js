a530207d671f805fc547fb2b52987385e745c1a3
will wade <willwade@gmail.com>
2025-11-04 14:00:22 +0000

    Fix: Replace __dirname with import.meta.url for ES modules
    - Add fileURLToPath import from 'url' module
    - Define __filename and __dirname using import.meta.url
    - Fixes ReferenceError in ES module scope
    - All examples now work correctly

--- examples/train-from-file.js
+++ examples/train-from-file.js
@@ -20,7 +20,12 @@
 
 import fs from 'fs';
 import path from 'path';
+import { fileURLToPath } from 'url';
 import { createPredictor, createErrorTolerantPredictor } from '../src/index.js';
+
+// Get __dirname equivalent in ES modules
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 console.log('='.repeat(60));
 console.log('Training from Files Example');
